6d4a60203ada4d30d66ef1d023d2a2fef740d12f
jisse Reitsma <jisse@yireo.com>
2023-11-09 11:45:18 +0100

    Fix component name

--- registration.php
+++ registration.php
@@ -3,4 +3,4 @@ declare(strict_types=1);
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Yireo/ExampleTheme', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/MaxServ/ExampleTheme', __DIR__);
